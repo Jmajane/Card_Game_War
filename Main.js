9faabe3310a910421ac168b6ae7cfa165937c564
Jmajane <jordan.majane@outlook.com>
2022-04-06 15:18:45 -0400

    Shuffle function created

--- Main.js
+++ Main.js
@@ -10,6 +10,16 @@ const ace = 14;
 // making the face cards have a const value of a number so they can be compared to other cards
 
 const cards = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]
-console.log(cards)
+// console.log(cards)
 // face cards show in the array with the const values assigned to them
 
+function start(){
+    // button to start the game and deal cards
+}
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5); // javascript.info/task/shuffle for shuffle function
+}
+shuffle(cards)
+console.log(cards)
+// shuffle function works
