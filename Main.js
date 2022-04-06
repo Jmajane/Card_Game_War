2a9f82004f634c9e5998a4bf8203d58a8194a7ed
Jmajane <jordan.majane@outlook.com>
2022-04-06 14:59:53 -0400

    Cards created and in Array

---
+++ Main.js
@@ -0,0 +1,15 @@
+let player = [];
+let computer = [];
+// player & computer will be empty arrays that get random cards pushed into them in a later function
+
+
+const jack = 11;
+const queen = 12;
+const king = 13;
+const ace = 14;
+// making the face cards have a const value of a number so they can be compared to other cards
+
+const cards = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]
+console.log(cards)
+// face cards show in the array with the const values assigned to them
+
