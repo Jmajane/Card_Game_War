e0b43c60c51ea36487c3fab87a568d6dfc58bcf5
Jmajane <jordan.majane@outlook.com>
2022-04-07 19:37:00 -0400

    Start Button Shuffles Cards

--- Main.js
+++ Main.js
@@ -22,54 +22,66 @@ const ace = 14;
 // making the face cards have a const value of a number so they can be compared to other cards
 
 const cards = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]
+
 // console.log(cards)
 // face cards show in the array with the const values assigned to them
-
 startButton.addEventListener('click', () => {
+    initializeGame()
     display.innerHTML = "Play by clicking draw"
 })
-shuffle(cards)
 
+function initializeGame() {
+    shuffle(cards)
+}
+
+playAgainButton.addEventListener('click', () => {
+    reset()      
+    initializeGame()
+})
 // button to start the game and deal cards
 
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5); // javascript.info/task/shuffle for shuffle function
+    for (let i = 0; i <= cards.length; i++) {
+        let randomCard = cards.splice(6, 7)
+        player.push(randomCard)
+        player = player.flat()
+    }
+    console.log(player)
+    for (let i = 0; i <= cards.length; i++) {
+            let randomCard = cards.splice(0, 6)
+            computer.push(randomCard)
+            computer = computer.flat()
+    }
+    console.log(computer)
 }
 // console.log(cards)
 // shuffle function works
 
 
-for (let i = 0; i <= cards.length; i++) {
-    let randomCard = cards.splice(6, 7)
-    player.push(randomCard)
-    player = player.flat()
-}
 // // the for loop is going through the array of cards
 // // splice opens the card array, we then pick a spot in the array and pick a desired amount after that
 // // we then push the cards into the player array and use flat to make one array
 
-console.log(player)
-
-for (let i = 0; i <= cards.length; i++) {
-    let randomCard = cards.splice(0, 6)
-    computer.push(randomCard)
-    computer = computer.flat()
-}
-console.log(computer)
 
 drawButton.addEventListener('click', () => {
-    challenge()
+    if (player.length > 0 && computer.length > 0) {
+        challenge()
+    } else {
+        winner()
+        playerOne.innerHTML = "Game Over"
+        computerOne.innerHTML = "Game Over"
+    }
     for (let i = 0; i <= computer.length; i++) {
         countCompCards.innerHTML = `Computer has ${[i]} cards`
     }
-        for (let j = 0; j <= player.length; j++) {   
-            countPlayerCards.innerHTML = `Player has ${[j]} cards`
-        }
-//     // action button to draw a card
+    for (let j = 0; j <= player.length; j++) {   
+        countPlayerCards.innerHTML = `Player has ${[j]} cards`
+    }
+    //     // action button to draw a card
 })
 
 function challenge() {
-    winner()
     playersCard = player.shift(0)
     playerOne.innerHTML = playersCard
     computersCard = computer.shift(0)
@@ -85,34 +97,39 @@ function challenge() {
         computer.push(computersCard)
         console.log(computer)
         display.innerHTML = "The Computer Wins"
-    // } else if (playersCard = computersCard) {
-    } else {
+        // } else if (playersCard = computersCard) {
+        } else {
+            return
+        }
         return
     }
-    return
-}
-
-function winner() {
-    if (player.length === 0 ) {
-        display.innerHTML = "The Computer Has Beat You"
-    } else if (computer.length === 0) {
-        display.innerHTML = "You Beat the COMPUTER"
-    } else {
-        return
+    
+    function winner() {
+        if (player.length === 0 ) {
+            display.innerHTML = "The Computer Has Beat You"
+        } else if (computer.length === 0) {
+            display.innerHTML = "You Beat the COMPUTER"
+        } else {
+            return
+        }
     }
+    
+    
+    // function battle() {
+        //     playersCard = player.shift(0, 1, 2, 3)
+        //     computersCard = computer.shift(0, 1, 2, 3)
+        //     if (playersCard[4] > playersCard[4]) {
+            
+            //     }
+            // }
+      
+function reset() {
+    player = []
+    computer = []
 }
-
-
-// function battle() {
-//     playersCard = player.shift(0, 1, 2, 3)
-//     computersCard = computer.shift(0, 1, 2, 3)
-//     if (playersCard[4] > playersCard[4]) {
-        
-//     }
-// }
-
-
-
-// console.log(countPlayerCards)
-
-
+            
+            
+            // console.log(countPlayerCards)
+            
+            
+            
